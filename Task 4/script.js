f6651ba8dda2c52d5be30f259c0b275f6c76cc05
Your Nameievukas256 <you@example.comsibailaite@gmail.com>
2022-08-30 22:35:01 +0300

    Task 4 done

--- Task 4/script.js
+++ Task 4/script.js
@@ -9,4 +9,46 @@ Pastaba: Sukurta kortelė, kurioje yra informacija apie automobilį (brand), tur
 turėti bent minimalų stilių ir būti responsive;
 -------------------------------------------------------------------------- */
 
-const ENDPOINT = 'cars.json';
+const ENDPOINT = "cars.json";
+
+fetch(ENDPOINT)
+  .then((Response) => Response.json())
+  .then((data) => {
+    console.log(data);
+    renderData(data);
+  });
+createTable();
+
+function createTable() {
+  const createTable = document.createElement("table");
+  document.getElementById("output").append(createTable);
+  const tBody = document.createElement("tbody");
+  const tHead = document.createElement("thead");
+  createTable.append(tHead, tBody);
+
+  const tr = document.createElement("tr");
+  tHead.append(tr);
+
+  const thBrand = document.createElement("th");
+  const thModels = document.createElement("th");
+  thBrand.textContent = "Brand";
+  thModels.textContent = "Models";
+
+  tr.append(thBrand, thModels);
+}
+
+function renderData(cars) {
+  const tbody = document.querySelector("tbody");
+  tbody.innerHTML = "";
+  cars.forEach((car) => {
+    const tr = document.createElement("tr");
+    const tdBrand = document.createElement("td");
+    const tdModels = document.createElement("td");
+
+    tdBrand.textContent = car.brand;
+    tdModels.textContent = car.models;
+
+    tbody.append(tr);
+    tr.append(tdBrand, tdModels);
+  });
+}
